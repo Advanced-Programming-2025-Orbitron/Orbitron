63d896e51a8e0c1bba3ccb9f7c8588c6fc575ea6
_*M <munhdul1601@gmail.com>
2025-12-04 21:24:09 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+use common_game::components::sunray::Sunray;
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ target/debug/deps/common.d
@@ -0,0 +1,5 @@
+C:\Users\DELL\Desktop\AP_project\target\debug\deps\common.d: src\main.rs
+
+C:\Users\DELL\Desktop\AP_project\target\debug\deps\common.exe: src\main.rs
+
+src\main.rs:

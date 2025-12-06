6ac953fe6e6f234a99c253a351d68e0dc6fff06e
_*M <munhdul1601@gmail.com>
2025-12-06 15:57:24 +0000

    sync

---
+++ target/debug/deps/common_game-817e38b7d6b14868.d
@@ -0,0 +1,7 @@
+/workspace/target/debug/deps/common_game-817e38b7d6b14868.d: src/main.rs src/ai.rs src/ai/orbitron.rs
+
+/workspace/target/debug/deps/libcommon_game-817e38b7d6b14868.rmeta: src/main.rs src/ai.rs src/ai/orbitron.rs
+
+src/main.rs:
+src/ai.rs:
+src/ai/orbitron.rs:
---
+++ target/debug/deps/log-46c9831855342d76.d
@@ -0,0 +1,8 @@
+/workspace/target/debug/deps/log-46c9831855342d76.d: /opt/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/lib.rs /opt/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/macros.rs /opt/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/serde.rs /opt/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/__private_api.rs
+
+/workspace/target/debug/deps/liblog-46c9831855342d76.rmeta: /opt/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/lib.rs /opt/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/macros.rs /opt/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/serde.rs /opt/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/__private_api.rs
+
+/opt/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/lib.rs:
+/opt/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/macros.rs:
+/opt/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/serde.rs:
+/opt/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/__private_api.rs:
